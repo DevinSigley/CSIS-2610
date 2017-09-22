2e68679f244b62ca190c240abbcef1a2baf49f32
DevinSigley <devinsigley@gmail.com>
2017-09-21 21:27:14 -0400

    Finished Assignment 3.

--- Assignment3/Assignment3/main.cpp
+++ Assignment3/Assignment3/main.cpp
@@ -4,9 +4,32 @@
 * It calculates a total when given a subtotal.
 */
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
+	const double SALES_TAX = 0.065;
+	double tax = 0.0,
+		   dblSubtotal = 0.0,
+		   total = 0.0;
+
+	// Asking the user for their subtotal input, with basic data validation.
+	cout << "This program calculates your total bill (tax included).\n";
+	do {
+		cout << "Please enter a valid subtotal (greater than zero): ";
+		cin >> dblSubtotal;
+	} while (dblSubtotal <= 0.0);
+	
+	// Calculating the added tax and subsequent total.
+	tax = dblSubtotal * SALES_TAX;
+	total = dblSubtotal + tax;
+
+	// Outputting the user's receipt in dollar form.
+	cout << fixed << setprecision(2);
+	cout << "Here is Your Receipt: \n";
+	cout << "Subtotal: $" << dblSubtotal << endl;
+	cout << "Tax: $" << tax << endl;
+	cout << "Total amount Due: $" << total << endl;
 	return 0;
 }
